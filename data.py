83d89bc6ddc50bdb61095e9524e0f5f209e1706c
SedatParlak <80989864+SedatParlak@users.noreply.github.com>
2021-05-04 22:13:07 +0300

    Update data.py

--- data.py
+++ data.py
@@ -4,7 +4,7 @@ question_data = []
 
 
 def get_questions():
-    response = requests.get("https://opentdb.com/api.php?amount=20&category=9")
+    response = requests.get("https://opentdb.com/api.php?amount=20&category=9&type=boolean")
     response.raise_for_status()
 
     data = response.json()
